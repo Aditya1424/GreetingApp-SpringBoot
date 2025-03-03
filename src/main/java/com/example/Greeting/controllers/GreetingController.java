daa8f9402048a6e9204c00272efd1c9c4bcbdf29
Aditya Gupta <ag7023587@gmail.com>
2025-03-03 15:00:36 +0530

    UC2 added

--- src/main/java/com/example/Greeting/controllers/GreetingController.java
+++ src/main/java/com/example/Greeting/controllers/GreetingController.java
@@ -1,11 +1,20 @@
 package com.example.Greeting.controllers;
 
 import com.example.Greeting.dto.MessageDTO;
+import com.example.Greeting.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+    @Autowired
+    GreetingService gs;
+
+//    This is constructor injection
+//    public GreetingController(GreetingService gs){
+//        this.gs= gs;
+//    }
 
     @GetMapping("/get")
     public String getGreetings(){
@@ -22,4 +31,10 @@ public class GreetingController {
         return "{\""+message+": \"Hello from PUT Request!\"}";
 
     }
+
+    public String serviceGreetings(){
+        return gs.getGreetings();
+    }
+
+
 }
